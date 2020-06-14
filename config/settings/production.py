662cc84d1869e61915137b5746cec378fd641627
Leigh Michael Forrest <leigh.m.data@gmail.com>
2020-06-14 12:04:41 -0400

    Logging, Anymail commented out

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,21 +18,6 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int(  # noqa: F405
     "CONN_MAX_AGE", default=60
 )
 
-# CACHES
-# ------------------------------------------------------------------------------
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": env("REDIS_URL"),
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#             # Mimicing memcache behavior.
-#             # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-#             "IGNORE_EXCEPTIONS": True,
-#         },
-#     }
-# }
-
 # SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
