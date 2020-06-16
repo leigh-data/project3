bb2be533c75a92c3c2bbe80b7e1dfe4f9645379e
Leigh Michael Forrest <leigh.m.data@gmail.com>
2020-06-16 16:35:56 -0400

    model admin enhancements for pizzas

--- project3/menu/admin.py
+++ project3/menu/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 
-from .models import PizzaTopping, PizzaMenuItem, SandwichMenuItem, OneSizeMenuItem, PlatterMenuItem
+from .models import (
+    PizzaTopping,
+    PizzaMenuItem,
+    SandwichMenuItem,
+    OneSizeMenuItem,
+    PlatterMenuItem,
+)
+
+
+@admin.register(PizzaMenuItem)
+class PizzaMenuItemAdmin(admin.ModelAdmin):
+    ordering = ("toppings", "pizza_type")
+    fields = (
+        "pizza_type",
+        "toppings",
+        "menu_description",
+        "cart_description",
+        "small_price",
+        "large_price",
+    )
+
 
 admin.site.register(PizzaTopping)
-admin.site.register(PizzaMenuItem)
 admin.site.register(SandwichMenuItem)
 admin.site.register(OneSizeMenuItem)
 admin.site.register(PlatterMenuItem)
--- project3/menu/models.py
+++ project3/menu/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
+
 class PizzaManager(models.Manager):
     def get_regular_pizzas(self):
-        return self.filter(pizza_type=PizzaMenuItem.PizzaType.REGULAR).order_by('toppings')
-    
+        return self.filter(pizza_type=PizzaMenuItem.PizzaType.REGULAR).order_by(
+            "toppings"
+        )
+
     def get_sicilian_pizzas(self):
-        return self.filter(pizza_type=PizzaMenuItem.PizzaType.SICILIAN).order_by('toppings')
+        return self.filter(pizza_type=PizzaMenuItem.PizzaType.SICILIAN).order_by(
+            "toppings"
+        )
 
 
 class OnseSizeMenuItemManager(models.Manager):
     def get_salads(self):
         return self.filter(entree_type=OneSizeMenuItem.OneSizeType.SALAD)
-    
+
     def get_pasta(self):
         return self.filter(entree_type=OneSizeMenuItem.OneSizeType.PASTA)
 
@@ -22,8 +27,14 @@ class PizzaTopping(models.Model):
 
 class MenuItem(models.Model):
     menu_description = models.CharField("Menu Description", max_length=126)
+    cart_description = models.CharField("Cart Description", max_length=126)
     large_price = models.DecimalField("Large Price", max_digits=5, decimal_places=2)
-    small_price = models.DecimalField("Small Price", max_digits=5, decimal_places=2, blank=True, null=True)
+    small_price = models.DecimalField(
+        "Small Price", max_digits=5, decimal_places=2, blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.cart_description
 
 
 class PizzaMenuItem(MenuItem):
@@ -34,15 +45,18 @@ class PizzaMenuItem(MenuItem):
         SICILIAN = ("Sicilian", "Sicilian")
 
     toppings = models.PositiveSmallIntegerField("Number of Toppings")
-    pizza_type = models.CharField("Pizza Type", max_length=50, choices=PizzaType.choices, default=PizzaType.REGULAR)
+    pizza_type = models.CharField(
+        "Pizza Type",
+        max_length=50,
+        choices=PizzaType.choices,
+        default=PizzaType.REGULAR,
+    )
 
     class Meta:
-        unique_together = ['pizza_type', 'toppings']
+        unique_together = ["pizza_type", "toppings"]
         verbose_name = "Pizza"
         verbose_name_plural = "Pizzas"
-    
-    def __str__(self):
-        return f"{self.menu_description} - {self.pizza_type}"
+
 
 class SandwichMenuItem(MenuItem):
     extra_cheese = models.BooleanField("Extra Cheese")
@@ -55,20 +69,25 @@ class SandwichMenuItem(MenuItem):
 
 
 class PlatterMenuItem(MenuItem):
-    
     class Meta:
         verbose_name = "Platter"
         verbose_name_plural = "Platters"
+
 
 class OneSizeMenuItem(MenuItem):
     class OneSizeType(models.TextChoices):
         PASTA = ("pasta", "Pasta")
         SALAD = ("salad", "Salad")
-    
+
     objects = OnseSizeMenuItemManager()
 
-    entree_type = models.CharField("Entree Type", max_length=50, choices=OneSizeType.choices, default=OneSizeType.SALAD)
-    
+    entree_type = models.CharField(
+        "Entree Type",
+        max_length=50,
+        choices=OneSizeType.choices,
+        default=OneSizeType.SALAD,
+    )
+
     class Meta:
         verbose_name = "One Size"
         verbose_name_plural = "One Size Entrees"
